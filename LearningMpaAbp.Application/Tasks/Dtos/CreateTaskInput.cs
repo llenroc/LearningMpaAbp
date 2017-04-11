e86e4ee8b5562d4f7232f495247d2332730149c8
web240 <240717653@qq.com>
2017-04-12 00:58:51 +0800

    update md and add application

---
+++ LearningMpaAbp.Application/Tasks/Dtos/CreateTaskInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMpaAbp.Tasks.Dtos
+{
+    public class CreateTaskInput
+    {
+        public int? AssignedPersonId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public TaskState State { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[CreateTaskInput > AssignedPersonId = {0}, Description = {1}]", AssignedPersonId, Description);
+        }
+}
---
+++ LearningMpaAbp.Application/Tasks/Dtos/GetTaskOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMpaAbp.Tasks.Dtos
+{
+    public class GetTaskOutput
+    {
+        public List<TaskDto> Tasks { get; set; }
+    }
+}
